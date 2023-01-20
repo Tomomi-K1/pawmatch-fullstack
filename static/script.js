9b58144749e1c87523a093e096fdd3512a9c5524
Tomomi-K1 <tomomi.music@gmail.com>
2023-01-19 19:24:04 -0800

    update match_result.html

--- static/script.js
+++ static/script.js
@@ -1,6 +1,14 @@
+const $readyBtn =$('.ready-btn');
+const $loader =$('.loader');
+
+
 const $matchDisplayArea =$('.match-display-area');
 const $matchedPet = $('.matched-pet');
 const $nextBtn = $('.nextbtn');
+
+$readyBtn.on('click', function{
+
+})
 
 // function hidePageCompornent(){
 //     $matchDisplayArea.hide()
